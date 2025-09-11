dfa82c9cff5c6f8f31f66e4cc406c0590f7e7fca
Yashodhan Gurav <guravyashodhan@gmail.com>
2025-09-11 22:27:59 +0530

    Delete MAJORPROJECT directory

--- MAJORPROJECT/utils/wrapAsync.js
+++
@@ -1,8 +0,0 @@
-module.exports = (fn)=> {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-}  
-
-// we are going to use this function to wrap all our async functions in the routes so that if any error occurs it will be passed to the next middleware which is the error handling middleware
-// This is a higher-order function that takes an async function (fn) as an argument
